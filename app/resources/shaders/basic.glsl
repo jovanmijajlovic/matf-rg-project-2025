0311107c2947b197255bb629e5af3a95dfb3d709
Jovan Mijajlovic <mijajlovicjox@gmail.com>
2026-08-04 14:34:31 +0200

    Update old shaders

--- app/resources/shaders/basic.glsl
+++ app/resources/shaders/basic.glsl
@@ -22,7 +22,8 @@ void main()
 
 //#shader fragment
 #version 330 core
-out vec4 FragColor;
+layout (location = 0) out vec4 FragColor;
+layout (location = 1) out vec4 BrightColor;
 in vec2 TexCoords;
 in vec3 Normal;
 in vec3 FragPos;
@@ -99,4 +100,9 @@ void main()
     for(int i = 0; i < LIGHTS_NUM; i++)
     result += CalcSpotLight(spotLights[i], norm, FragPos, viewDir, TexCoords);
     FragColor = vec4(result, 1.0);
+    float brightness = dot(result, vec3(0.2126, 0.7152, 0.0722));
+    if(brightness > 1.0)
+    BrightColor = vec4(result, 1.0);
+    else
+    BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
