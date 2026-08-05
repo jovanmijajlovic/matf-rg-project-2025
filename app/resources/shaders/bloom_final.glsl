683e8cdf6d21cdf773d5ba2c9372b5e382a4762f
Jovan Mijajlovic <mijajlovicjox@gmail.com>
2026-08-05 23:16:39 +0200

    Change bulb to be oval

--- app/resources/shaders/bloom_final.glsl
+++ app/resources/shaders/bloom_final.glsl
@@ -18,12 +18,10 @@ uniform bool bloom;
 uniform float exposure;
 void main()
 {
-    const float gamma = 2.2;
     vec3 hdrColor = texture(scene, TexCoords).rgb;
     vec3 bloomColor = texture(bloomBlur, TexCoords).rgb;
     if(bloom)
     hdrColor += bloomColor;
     vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
-    result = pow(result, vec3(1.0 / gamma));
     FragColor = vec4(result, 1.0);
 }
